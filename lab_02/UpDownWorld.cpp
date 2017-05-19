ad5585d51053d40e562204e608c7f8bc05613a29
harrybao <13003909146@sina.cn>
2017-05-19 19:59:29 +0800

    Experiment two

---
+++ lab_02/UpDownWorld.cpp
@@ -0,0 +1,93 @@
+#include <cstring> 
+#include <cstdlib> 
+#include <iostream>
+using namespace std;
+
+char str[10];  
+int j=0;  
+void E();           //E->TX;  
+void X();           //X->+TX | e  
+void T();           //T->FY  
+void Y();           //Y->*FY | e  
+void F();           //F->(E) | i  
+int main()  
+{  
+    int len;  
+    cout <<"请输入算数表达式：";  
+    cin>>str;  
+    len=strlen(str);  
+    str[len]='#';  
+    str[len+1]='\0';  
+    E();  
+    cout <<"正确语句！"<<endl;  
+    strcpy(str,"");  
+    j=0;   
+}  
+void E()  
+{  
+	cout <<"E--->TE'    " <<endl;
+    T();  
+    X();  
+}  
+void X()  
+{  
+    if(str[j]=='+')  
+    {  
+    	cout<<"E'--->+TE'   " <<endl;
+    	cout<<"    匹配+"<<endl;
+        j++;  
+        T();  
+        X();  
+    }
+	else
+		cout<<"E'--->ε       " <<endl;   
+}  
+void T()  
+{  
+	cout<<"T--->FT'    " <<endl;
+    F();  
+    Y();  
+}  
+void Y()  
+{  
+    if(str[j]=='*')  
+    {  
+    	cout<<"T--->*FT'    " <<endl;
+    	cout<<"    匹配*"<<endl;
+        j++;  
+        F();  
+        Y();  
+    } else
+		cout<<"T'--->ε       " <<endl; 
+}  
+void F()  
+{  
+    if(str[j]=='i')  
+    {  
+    	cout<<"F--->i       " <<endl;
+    	cout<<"    匹配i"<<endl;
+        j++;  
+    }  
+    else if (str[j]=='(')  
+    {   
+		cout<<"F--->(E)    " <<endl;   
+        j++;  
+        E();
+		while(str[j]!=')'){
+			cout<<"error:never find ')'"<<endl;
+			exit(0);
+		}  
+        if(str[j]==')')  
+        {  
+        	cout<<"F--->(E)    " <<endl;
+            j++;   
+        }else{  
+            cout<<"\n分析失败!"<<endl;  
+            exit (0);  
+        }  
+    }   
+    else{  
+        cout<<"分析失败!"<<endl;   
+        exit(0);  
+    }  
+ }  
