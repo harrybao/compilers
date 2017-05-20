73167b97277248b5017d9f2167a5bd27a2a61e00
harrybao <13003909146@sina.cn>
2017-05-20 11:15:50 +0800

    rewritng with python

--- lab_02/UpDown.py
+++ lab_02/UpDown.py
@@ -1,54 +1,54 @@
 #coding:utf-8
 j=0
 def E():
-	print "E -->  TE'"
-	T()
-	X()
+    print "E -->  TE'"
+    T()
+    X()
 def X():
-	global j
-	if(str[j]=='+'):
-		print "E' -->  +TE'   Mate to +"
-		j+=1
-		T()
-		X()
-	else:
-		print "E' -->  ε"
+    global j
+    if(str[j]=='+'):
+        print "E' -->  +TE'   Mate to +"
+        j+=1
+        T()
+        X()
+    else:
+        print "E' -->  ε"
 def T():
-	print "T -->  FT'"
-	F()
-	Y()
+    print "T -->  FT'"
+    F()
+    Y()
 def Y():
-	global j
-	if(str[j]=='*'):
-		print "T' -->  *FT'   Mate to *"
-		j+=1
-		F()
-		Y()
-	else:
-		print "T' -->  ε"
+    global j
+    if(str[j]=='*'):
+        print "T' -->  *FT'   Mate to *"
+        j+=1
+        F()
+        Y()
+    else:
+        print "T' -->  ε"
 def F():
-	global j
-	if(str[j]=='i'):
-		print "F -->  i       Mate to i"
+    global j
+    if(str[j]=='i'):
+        print "F -->  i       Mate to i"
+        j+=1
+    elif(str[j]=='('):
+        print "F -->  (E)"
+        j+=1
+        E()
+        while(str[j]!=')'):
+            print "[ERROR]: Never find ')'!"
+            exit(0)
+	    if(str[j]==')'):
 		j+=1
-	elif(str[j]=='('):
-		print "F -->  (E)"
-		j+=1
-		E()
-		while(str[j]!=')'):
-			print "[ERROR]: never find ')'!"
-			exit(0)
-		if(str[j]==')'):
-			j+=1
-		else:
-			print "Alalyzing errors!"
-			exit(0)
-	else:
-		print "Alalyzing errors!"
+	    else:
+        	print "[ERROR]: Alalyzing errors!"
 		exit(0)
+    else:
+        print "[ERROR}: Alalyzing errors!"
+        exit(0)
 if __name__=='__main__':
-	str=input('please input string:')
-	str=str+'$'
-	E()
-	if(str[j]=='$'):
-		print "The input is true"
+    str=input('please input string:')
+    str=str+'$'
+    E()
+    if(str[j]=='$'):
+        print "[RIGHT]: The input is true"
