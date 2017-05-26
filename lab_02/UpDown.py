9a19178c07f3abe1506a3f4aa16d262fd5b7d17a
harrybao <13003909146@sina.cn>
2017-05-26 17:02:32 +0800

    revise

--- lab_02/UpDown.py
+++ lab_02/UpDown.py
@@ -1,20 +1,19 @@
 #coding:utf-8
 
 def E():
-    global k
-    if(str1[k]=='i' or str1[k]=='('):
-        print "E -->  TE'"
-        T()
-        X()
-    else:
-        print "[ERROR]: Never find 'i' or '('!"
-        exit(0)
+    print "E -->  TE'"
+    T()
+    X()
 
 def X():
-    global k
+    global k,temp
     if(str1[k]=='+'):
         print "E' -->  +TE'   Mate to +"
         k+=1
+        if(str1[k]!='i' and str1[k]!='('):
+            print k,"[ERROR]: Never find 'i' or '('!"
+            temp=1
+            k+=1
         T()
         X()
     else:
@@ -26,37 +25,52 @@ def T():
     Y()
 
 def Y():
-    global k
+    global k,temp
     if(str1[k]=='*'):
         print "T' -->  *FT'   Mate to *"
         k+=1
+        if(str1[k]!='i' and str1[k]!='('):
+            print k,"[ERROR]: Never find 'i' or '('!"
+            temp=1
+            k+=1
         F()
         Y()
     else:
         print "T' -->  ε"
 
 def F():
-    global k
+    global k,temp
     if(str1[k]=='i'):
         print "F -->  i       Mate to i"
         k+=1
         if(str1[k]!='+' and str1[k]!='*' and str1[k]!=')' and str1[k]!='$'):
-            print "[ERROR]: Never find '+' or '*'"
-            exit(0)
+            print k,"[ERROR]: Never find '+' or '*'"
+            temp=1
+            if(str1[k]=='(' or str1[k]=='i'):
+                F()
     elif(str1[k]=='('):
         print "F -->  (E)"
         k+=1
         E()
         while(str1[k]!=')'):
-            print "[ERROR]: Never find ')'!"
-            exit(0)
+            print k,"[ERROR]: Never find ')'!"
+            temp=1
+            break 
         if(str1[k]==')'):
             k+=1
+    else:
+        print k,"[ERROR]: Never find 'i' or '('!"
+        temp=1
+        k+=1
 
 if __name__=='__main__':
     str1=input('please input string:')
     str1=str1+'$'
     k=0
+    temp=0
     E()
     if(str1[k]=='$'):
-        print "[RIGHT]: The input is true"
+        if(temp==0):
+            print "[RIGHT]: The input is true!"
+        else:
+            print "[FLASE]: This is error string!"
