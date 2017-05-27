6865a859c2709eb34f0cfa26bea1c9eee1f38759
harrybao <13003909146@sina.cn>
2017-05-27 19:44:11 +0800

    rewriting

--- lab_03/LL1.py
+++ lab_03/LL1.py
@@ -2,9 +2,10 @@
 
 
 def LL1():
+	global c 
 	if(c=='E'):
 		if(a[i]=='i' or a[i]=='('):
-			print "E->TE'"
+			print stack,"E->TE'",a[i:]
 			stack.append('e')
 			stack.append('T')
 			c=stack.pop()
@@ -12,19 +13,19 @@ def LL1():
 			error()
 	elif(c=='e'):
 		if(a[i]=='+'):
-			print "E'->+TE'"
+			print stack,"E'->+TE'",a[i:]
 			stack.append('e')
 			stack.append('T')
 			stack.append('+')
 			c=stack.pop()
 		elif(a[i]==')' or a[i]=='$'):
-			print "E'->&"
+			print stack,"E'->&",a[i:]
 			c=stack.pop()
 		else:
 			error()
 	elif(c=='T'):
 		if(a[i]=='i' or a[i]=='('):
-			print "T->FT'"
+			print stack,"T->FT'",a[i:]
 			stack.append('t')
 			stack.append('F')
 			c=stack.pop()
@@ -32,10 +33,10 @@ def LL1():
 			error()
 	elif(c=='t'):
 		if(a[i]=='+' or a[i]==')' or a[i]=='$'):
-			print "T'->&"
+			print stack,"T'->&",a[i:]
 			c=stack.pop()
 		elif(a[i]=='*'):
-			print "T'->*FT'"
+			print stack,"T'->*FT'",a[i:]
 			stack.append('t')
 			stack.append('F')
 			stack.append('*')
@@ -44,11 +45,11 @@ def LL1():
 			error()
 	elif(c=='F'):
 		if(a[i]=='i'):
-			print "F->i"
+			print stack,"F->i",a[i:]
 			stack.append('i')
 			c=stack.pop()
 		elif(a[i]=='('):
-			print "F->(E)"
+			print stack,"F->(E)",a[i:]
 			stack.append(')')
 			stack.append('E')
 			stack.append('(')
@@ -57,20 +58,15 @@ def LL1():
 			error()
 
 def error():
+	global i 
 	print i+1,a[i],"error"
 	i+=1
 
 if __name__ == '__main__':
-	in1=raw_input("Input the syant:")
-	z=chr(in1)
+	a=raw_input("Input Expression:")
+	a=a+'$'
 	i=0
-	z=0
-	n=0
-	a=""
-	while(z!='$'):
-		a[n]=z
-		n+=1
-		a[n]='$'
+	stack=[]
 	stack.append('$')
 	stack.append('E')
 	c=stack.pop()
