bc6e6887bfd1acebefafc7846fe507058fc8df0b
harrybao <13003909146@sina.cn>
2017-05-28 21:11:44 +0800

    rewriting

--- lab_03/LL1.py
+++ lab_03/LL1.py
@@ -1,43 +1,43 @@
 #coding:utf-8
-
+from prettytable import PrettyTable
 
 def LL1():
 	global c 
 	if(c=='E'):
 		if(a[i]=='i' or a[i]=='('):
-			print stack,"E->TE'",a[i:]
-			stack.append('e')
+			x.add_row([stack,a[i:], "E -> TE'"])
+			stack.append('X')
 			stack.append('T')
 			c=stack.pop()
 		else:
 			error()
-	elif(c=='e'):
+	elif(c=='X'):
 		if(a[i]=='+'):
-			print stack,"E'->+TE'",a[i:]
-			stack.append('e')
+			x.add_row([stack,a[i:], "E' -> +TE'"])
+			stack.append('X')
 			stack.append('T')
 			stack.append('+')
 			c=stack.pop()
 		elif(a[i]==')' or a[i]=='$'):
-			print stack,"E'->&",a[i:]
+			x.add_row([stack,a[i:], "E' -> ε"])
 			c=stack.pop()
 		else:
 			error()
 	elif(c=='T'):
 		if(a[i]=='i' or a[i]=='('):
-			print stack,"T->FT'",a[i:]
-			stack.append('t')
+			x.add_row([stack,a[i:], "T -> FT'"])
+			stack.append('Y')
 			stack.append('F')
 			c=stack.pop()
 		else:
 			error()
-	elif(c=='t'):
+	elif(c=='Y'):
 		if(a[i]=='+' or a[i]==')' or a[i]=='$'):
-			print stack,"T'->&",a[i:]
+			x.add_row([stack,a[i:], "T' -> ε"])
 			c=stack.pop()
 		elif(a[i]=='*'):
-			print stack,"T'->*FT'",a[i:]
-			stack.append('t')
+			x.add_row([stack,a[i:], "T' -> *FT'"])
+			stack.append('Y')
 			stack.append('F')
 			stack.append('*')
 			c=stack.pop()
@@ -45,11 +45,11 @@ def LL1():
 			error()
 	elif(c=='F'):
 		if(a[i]=='i'):
-			print stack,"F->i",a[i:]
+			x.add_row([stack,a[i:], "F -> i"])
 			stack.append('i')
 			c=stack.pop()
 		elif(a[i]=='('):
-			print stack,"F->(E)",a[i:]
+			x.add_row([stack,a[i:], "F -> (E)"])
 			stack.append(')')
 			stack.append('E')
 			stack.append('(')
@@ -57,15 +57,21 @@ def LL1():
 		else:
 			error()
 
+
 def error():
 	global i 
-	print i+1,a[i],"error"
+	x.add_row([i+1,a[i], "error"])
 	i+=1
 
 if __name__ == '__main__':
 	a=raw_input("Input Expression:")
 	a=a+'$'
 	i=0
+	x = PrettyTable(["Stack", "Input", "Action"])
+	x.padding_width = 1
+	x.align["Stack"] = "l"
+	x.align["Input"] = "r"
+	x.align["Action"] = "l"
 	stack=[]
 	stack.append('$')
 	stack.append('E')
@@ -78,4 +84,4 @@ if __name__ == '__main__':
 			break
 		else:
 			LL1()
-	
+	print x
