68964370210ffd31618cd108ae3f43916eb0caf6
harrybao <13003909146@sina.cn>
2017-05-30 09:30:07 +0800

    last rewrited

--- lab_03/LL1analyse.py
+++ lab_03/LL1analyse.py
@@ -5,58 +5,66 @@ def LL1():
 	global c,flag
 	if(c=='E'):
 		if(a[k]=='i' or a[k]=='('):
-			table.add_row([stack,a[k:], "E -> TE'"])
+			table.add_row([a[k:], "E -> TE'"])
 			stack.append('X')
 			stack.append('T')
+			print stack
 			c=stack.pop()
 		else:
 			error()
 			flag=1
 	elif(c=='X'):
 		if(a[k]=='+'):
-			table.add_row([stack,a[k:], "E' -> +TE'"])
+			table.add_row([a[k:], "E' -> +TE'"])
 			stack.append('X')
 			stack.append('T')
 			stack.append('+')
+			print stack
 			c=stack.pop()
 		elif(a[k]==')' or a[k]=='$'):
-			table.add_row([stack,a[k:], "E' -> ε"])
+			table.add_row([a[k:], "E' -> ε"])
+			print stack
 			c=stack.pop()
 		else:
 			error()
 			flag=1
 	elif(c=='T'):
 		if(a[k]=='i' or a[k]=='('):
-			table.add_row([stack,a[k:], "T -> FT'"])
+			table.add_row([a[k:], "T -> FT'"])
 			stack.append('Y')
 			stack.append('F')
+			print stack
 			c=stack.pop()
 		else:
 			error()
 			flag=1
 	elif(c=='Y'):
 		if(a[k]=='*'):
-			table.add_row([stack,a[k:], "T' -> *FT'"])
+			table.add_row([a[k:], "T' -> *FT'"])
 			stack.append('Y')
 			stack.append('F')
 			stack.append('*')
+			print stack
 			c=stack.pop()
 		elif(a[k]=='+' or a[k]==')' or a[k]=='$'):
-			table.add_row([stack,a[k:], "T' -> ε"])
+			table.add_row([a[k:], "T' -> ε"])
+			print stack
 			c=stack.pop()	
 		else:
 			error()
 			flag=1
 	elif(c=='F'):
 		if(a[k]=='i'):
-			table.add_row([stack,a[k:], "F -> i"])
+			table.add_row([a[k:], "F -> i"])
 			stack.append('i')
+			print stack
 			c=stack.pop()
 		elif(a[k]=='('):
-			table.add_row([stack,a[k:], "F -> (E)"])
+			table.add_row([a[k:], "F -> (E)"])
 			stack.append(')')
 			stack.append('E')
 			stack.append('(')
+			print stack
 			c=stack.pop()
 		else:
 			error()
@@ -66,7 +74,8 @@ def LL1():
 
 def error():
 	global k 
-	table.add_row([k+1,a[k], "Error"])
+	table.add_row([a[k], "Error"])
+	print stack
 	k+=1
 
 if __name__ == '__main__':
@@ -75,10 +84,9 @@ if __name__ == '__main__':
 	for temp in a:
 		if(temp!='i' and temp!='+' and temp!='*' and temp!='(' and temp!=')' and temp!='$'):
 			print "You input is wrong!"
-			etableit(0)
-	table = PrettyTable(["Stack", "Input", "Action"])
+			exit(0)
+	table = PrettyTable(["Input", "Action"])
 	table.padding_width = 1
-	table.align["Stack"] = "l"
 	table.align["Input"] = "r"
 	table.align["Action"] = "l"
 	stack=[]
@@ -86,11 +94,13 @@ if __name__ == '__main__':
 	flag=0
 	stack.append('$')
 	stack.append('E')
-	table.add_row([stack,a[k:]," "])
+	table.add_row([a[k:]," "])
+	print stack
 	c=stack.pop()
 	while(c!='$'):
 		if(c==a[k]):
-			table.add_row([stack,a[k+1:],'{'+a[k]+'}'+" Matching"])
+			table.add_row([a[k+1:],'{'+a[k]+'}'+" Matching"])
+			print stack
 			k+=1
 			c=stack.pop()
 		elif(c==')' and c!=a[k]):
